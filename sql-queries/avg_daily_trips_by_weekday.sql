d86dcfac285be28751f5f384c0874fcc11460f88
yash-p-singh <yashpsingh.work@gmail.com>
2026-01-06 22:46:31 +0530

    Update avg_daily_trips_by_weekday.sql

--- sql-queries/avg_daily_trips_by_weekday.sql
+++ sql-queries/avg_daily_trips_by_weekday.sql
@@ -2,7 +2,7 @@ SELECT
   EXTRACT(DAYOFWEEK FROM started_at) AS weekday,
   CAST(COUNT(ride_id) / 52 AS INT) AS count_rides
 FROM
-  hello-476408.CyclisticTrips.BikeTrips
+  BikeTrips
 WHERE
   member_casual = 'casual'
 GROUP BY
