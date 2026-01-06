9ed62d3bcbbd65b321dd5d14ec077832ada82bb4
yash-p-singh <yashpsingh.work@gmail.com>
2026-01-06 22:45:22 +0530

    Update total_daily_trips_by_month.sql

--- sql-queries/total_daily_trips_by_month.sql
+++ sql-queries/total_daily_trips_by_month.sql
@@ -2,7 +2,7 @@ SELECT
   EXTRACT(MONTH FROM started_at) AS ride_month,
   COUNT(ride_id) AS count_rides
 FROM
-  hello-476408.CyclisticTrips.BikeTrips
+  BikeTrips
 WHERE
   member_casual = 'casual'
 GROUP BY
