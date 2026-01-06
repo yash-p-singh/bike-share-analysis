a89149f0d575825cd2f55399055dced6764d1802
yash-p-singh <yashpsingh.work@gmail.com>
2026-01-06 22:44:18 +0530

    Add files via upload

---
+++ sql-queries/avg_trip_duration_by_month.sql
@@ -0,0 +1,13 @@
+SELECT
+  EXTRACT(MONTH FROM started_at) AS month,
+  ROUND(AVG(EXTRACT(MINUTE FROM (ended_at - started_at)) 
+          + EXTRACT(HOUR FROM (ended_at - started_at)) * 60
+          + EXTRACT(SECOND FROM (ended_at - started_at)) / 60), 2) AS avg_trip_duration_minutes
+FROM
+  hello-476408.CyclisticTrips.BikeTrips
+WHERE
+  member_casual = 'casual'
+GROUP BY
+  month
+ORDER BY
+  month
